cd43b9aeb143d3c537c874360f31f27fc1345d5e
Production <production@ysjournal.com>
2020-03-01 14:13:16 +0100

    Added web pages

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session')
 const crypto = require('crypto');
+
+console.log('Running server')
+app.engine('view engine', require('ejs').renderFile);
+app.use(express.static('public'));
+
+app.post('/login', (req, res) => {
+    res.render(path.join(__dirname+'/views/index.ejs'), {submissions:names, requestedUser:submissions[0]});
+});
+
+app.get('*',function(req,res){
+    res.render(path.join(__dirname+'/views/login.ejs'));
+});
+
+
+app.listen(process.env.PORT || 8080);
