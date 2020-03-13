30d12c992502348ac25f5b1af4c340af1d7b5ab5
Young Scientists Journal <production@ysjournal.com>
2020-03-13 17:19:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
-  maxAge: 24 * 60 * 60 * 1000;
+  maxAge: 24 * 60 * 60 * 1000
 }))
 
 app.use('/', webplatform);
