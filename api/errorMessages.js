d9d2a32953f5a5b24503f6a99bcf14602a602cd0
flynn cowell <cowellflynn@gmail.com>
2020-03-17 12:43:34 +0000

    Change technicalsupport to technical issues

--- api/errorMessages.js
+++ api/errorMessages.js
@@ -1,6 +1,6 @@
 module.exports = {
 	errorMessage: function(code) {
-		let messages = { 401: 'You don\'t have access here. If you think it\'s an error contact technicalsupport@ysjournal.com'};
+		let messages = { 401: 'You don\'t have access here. If you think it\'s an error contact technicalsissues@ysjournal.com'};
 		return messages[code];
 	}
 };
