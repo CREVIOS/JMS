9c7c3e185b2d39c586b8b9395f636a94501b8a61
Federico Galbiati <fede.galbi@hotmail.com>
2020-03-23 02:13:03 +0100

    Cleaned

--- api/uploader.js
+++
@@ -1,9 +0,0 @@
-// const fs = require('fs');
-// const drive = require('./../api/googledrive.js');
-
-// module.exports = {
-// 	saveImage: function(req, res) {
-// 		console.log(req.files);
-// 		drive.uploadFile(req.body.title, req.files.imageId.tempFilePath, req, res);
-// 	}
-// };
